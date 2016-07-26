d2e4d9464edc2df87c429cbed59afd2c27c23226
Fran Acien <amil101ftw@gmail.com>
2016-07-27 01:16:52 +0200

    adding moreAudios script

--- AudioMixer/VLCMoreAudios.py
+++ AudioMixer/VLCMoreAudios.py
@@ -8,10 +8,14 @@ def threadFunction(filename):
     mediaplayer = instance.media_player_new()
 
     media = instance.media_new(
-        os.path.normpath(filename))
+        os.path.normpath(os.getcwd() + "/audio/" + filename))
     mediaplayer.set_media(media)
     mediaplayer.play()
 
-t = threading.Thread(target=threadFunction, args=())
+t = threading.Thread(target=threadFunction, args=("song1.mp3",), name="first")
+s = threading.Thread(target=threadFunction, args=("song0.mp3",), name="second")
+
+t.start()
+s.start()
 
 time.sleep(10)
